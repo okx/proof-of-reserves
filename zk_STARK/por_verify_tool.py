ca9f1b4f934fff5e7e60a5a47eec4629777b03fc
quan.li <quan.li@okg.com>
2023-04-25 17:12:27 +0800

    add zk stark

---
+++ zk_STARK/por_verify_tool.py
@@ -0,0 +1,41 @@
+from mk_and_verify_proofs import *
+from utils import * 
+from constants import *
+import json
+import sys
+import time
+import re
+
+def verify_sum_proofs():
+    abs_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    start_time = time.time()
+    batches_proof_path = abs_dir + "/sum_proof_data/batches/"
+    sum_values = [0] * (len(COINS) + 1)
+    for root, dirs, files in os.walk(batches_proof_path):
+        for dir in dirs:
+            result = verify_batch_proof(batches_proof_path + dir + "/")
+            for i in range(len(sum_values)):
+                sum_values[i] = (sum_values[i] + result[i]) % MODULUS
+            print("Sum Proof of Batch %s Verified" %dir)
+
+    trunk_proof_path = abs_dir + "/sum_proof_data/trunk/"
+    result = verify_trunk_proof(trunk_proof_path)
+    for i in range(len(sum_values)):
+        assert sum_values[i] == (result[i] % MODULUS)
+    print("Sum Proof of Trunk Verified")
+
+    print("All Proofs Verified in %.4f secs!" %(time.time() - start_time))
+
+def verify_all_inclusion_proof():
+    start_time = time.time()
+    abs_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    for root, dirs, files in os.walk(abs_dir+ "/inclusion_proof_data"):
+        for dir in dirs:
+            verify_inclusion_proof(abs_dir + "/" + dir + "/")
+            print("Inclusion Proof %s Verified" %dir)
+
+    print("All Proofs Verified in %.4f secs!" %(time.time() - start_time))
+
+if __name__ == '__main__':
+    verify_sum_proofs()
+    verify_all_inclusion_proof()
---
+++ zk_STARK/zk_STARK_Validator.py
@@ -0,0 +1,69 @@
+from mk_and_verify_proofs import verify_batch_proof, verify_trunk_proof, verify_single_inclusion_proof
+from constants import MODULUS, COINS
+import json
+import sys
+import re
+import os
+
+SUM_PROOF_PASS_INFO = "Total sum and non-negative constraint validation passed"
+SUM_PROOF_FAIL_INFO = "Total sum and non-negative constraint validation failed"
+INCLUSION_PROOF_PASS_INFO = "Inclusion constraint validation passed"
+INCLUSION_PROOF_FAIL_INFO = "Inclusion constraint validation failed"
+
+
+
+def por_user_verify_proofs():
+    print("============ Validation started ==============")
+    abs_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    for files in os.listdir(abs_dir):
+        if (files == "sum_proof_data"):
+            batches_proof_path = abs_dir + "/sum_proof_data/batches/"
+            sum_values = [0] * (len(COINS) + 1)
+            result = [0] * (len(COINS) + 1)
+            success = True
+            # verify batch proofs
+            for root, dirs, subfiles in os.walk(batches_proof_path):
+                for dir in dirs:
+                    try:
+                        result = verify_batch_proof(batches_proof_path + dir + "/")
+                    except:
+                        success = False
+                        break
+                    for i in range(len(sum_values)):
+                        sum_values[i] = (sum_values[i] + result[i]) % MODULUS
+            
+            # verify trunk proofs
+            if success:
+                trunk_proof_path = abs_dir + "/sum_proof_data/trunk/"
+                try:
+                    result = verify_trunk_proof(trunk_proof_path)
+                except:
+                    success = False
+            
+            # check the consistence between batches and trunk
+            if success:
+                for i in range(len(sum_values)):
+                    if sum_values[i] != (result[i] % MODULUS):
+                        success = False
+            # sum and non-negativity verification pass
+            if success:
+                print(SUM_PROOF_PASS_INFO)
+            else:
+                print(SUM_PROOF_FAIL_INFO)
+
+
+
+        if re.search("inclusion_proof.json", files):
+            try:
+                verify_single_inclusion_proof(abs_dir + "/" + files)
+                print(INCLUSION_PROOF_PASS_INFO)
+
+            except:
+                print(INCLUSION_PROOF_FAIL_INFO)
+    
+    print("============ Validation finished =============")
+
+
+
+if __name__ == '__main__':
+    por_user_verify_proofs()
